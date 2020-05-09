6fb4584e492e2dd55f705bfefe6f77749f4926e5
hoodielive <hoodielive@gmail.com>
2020-05-08 23:33:20 -0400

    maybe

--- UserTypes/union-variant.cpp
+++ UserTypes/union-variant.cpp
@@ -12,4 +12,7 @@ int main()
 	Variant v;
 	v.integer = 42;
 	printf("This is the contents of our integer: %d\n", v.integer);
+	v.floating_point = 3.1457;
+	printf("And now there is a floating point: %d\n", v.floating_point);
+
 }
