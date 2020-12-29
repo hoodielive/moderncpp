03f4423e65ba2949ae2453f0ccf315de9a1c847f
hoodielive <hoodielive@gmail.com>
2020-12-28 20:46:38 -0500

    Lord, I have backslidden from C++ again.. lol

--- ehh/arrayology.cpp
+++ ehh/arrayology.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include <array>
 
+int main()
+{
+    int myArray[3]{ 3 };
+
+    
+    for(size_t i = 0; i < 3; i++)
+    {
+        std::cout << myArray[2] << std::endl;
+    }
+    
+    return 0;
+}
