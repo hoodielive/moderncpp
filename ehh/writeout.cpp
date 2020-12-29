b4f4e8299d736a89fb45419194f57405009e14c1
hoodielive <hoodielive@gmail.com>
2020-12-28 20:18:33 -0500

    Lord, I have backslidden from C++ again.. lol

--- ehh/writeout.cpp
+++ ehh/writeout.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 // Output to file, if file does not exist, create it.
 // Text or Binary.
 
 int main()
 {
+    int age = 23;
+    double height = 1.75;
+
     std::string data = "This is data. \n This is a new Line.";
-    std::string fileName = "myFile.txt";
+    std::string name = "Larry";
+    std::string address = "Somewhere in the world.";
+    std::string fileName = "myPersonDatabase.txt";
+
     std::ofstream outFile;
 
     outFile.open(fileName.c_str());
 
     if (outFile.is_open())
     {
-        std::cout << data.c_str() << std::endl;
+        outFile << name 
+        <<  "\n" 
+        << age 
+        << "\n"
+        << address 
+        << "\n"
+        << height 
+        << "\n";
     }
 
     outFile.close();
