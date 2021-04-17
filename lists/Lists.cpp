f0269f7acefe106fad92a67202c75d09b40d2210
hoodielive <hoodielive@gmail.com>
2021-04-17 01:01:58 -0400

    Ohtay

--- lists/Lists.cpp
+++ lists/Lists.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <list>
 
+using namespace std;
+
+int main()
+{
+	list<int> numbers; 
+
+	numbers.push_back(1);
+	numbers.push_back(2);
+	numbers.push_back(3);
+	numbers.push_front(0);
+
+	list<int>::iterator it=numbers.begin();
+	it++;
+	numbers.insert(it, 100);
+
+	cout << "Element: " << *it << endl;
+
+	for (list<int>::iterator it=numbers.begin(); it != numbers.end(); it++)
+	{
+		cout << *it << endl;
+	}
+}
