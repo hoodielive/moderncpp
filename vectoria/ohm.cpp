6e3447e6fd5885e16ed967d5a5782cf1f441f4bc
hoodielive <hoodielive@gmail.com>
2021-04-23 00:14:05 -0400

    Vectoria..

--- vectoria/ohm.cpp
+++ vectoria/ohm.cpp
@@ -1,17 +1,17 @@
-#include <iostream> 
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
+vector<int> brittany = {1, 2, 3, 4, 5};
 void print(const vector<int>& my_vector)
 {
 	cout << "Vector content:  {  ";
 	cout << "}\n\n";
 }
 
-int main() 
+int main()
 {
-	vector<int> brittany = {1, 2, 3, 4, 5};
 	print(brittany);
 	return 0;
 }
