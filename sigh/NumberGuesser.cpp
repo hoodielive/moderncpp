203f67f0c7a66b588f07409b5164467a8c64ac26
hoodielive <hoodielive@gmail.com>
2021-05-31 16:16:27 -0400

    The condition

---
+++ sigh/NumberGuesser.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/*
+  Create a program called guesser. 
+  There will be a secret number that is set at the start of the program without the user's input.
+  The secret number should be picked from a certain range.
+  The range of the number should be output to the player.
+  The player should be prompted to enter his guess.
+  The prompt should show how many guesses the player has left.
+  The player will then enter his guess. If the guess is too high then the game will output that the guess was too high.
+  If the guess was too low then the game will output that it was too low.
+  The player only has a certain number of guesses.
+  The number of guesses should be based on the range of the secret number.
+  Any wrong guess will decrement the number of guesses left.
+  The game ends by saying what the secret numnber was regardless of if the player guessed it right or not.
+  When the game is over, the player should be prompted if he would like to play again.
+  If yes then the secret number should change.
+  Any input errors by the player should have proper handling.
+*/
