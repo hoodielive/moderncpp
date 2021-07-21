f70445d50d94f9bffe06bc0a2ec9ead2f91f074d
hoodielive <hoodielive@gmail.com>
2021-07-21 02:46:21 -0400

    Deleted and Default Functions

---
+++ cook/_deleted_defaulted.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+class foo_not_copyable
+{
+  public:
+    foo_not_copyable() = default;
+
+    foo_not_copyable(foo_not_copyable const&) = delete;
+    foo_not_copyable& operator=(foo_not_copyable const&) = delete;
+};
+
+class Data
+{
+
+};
+
+class data_wrapper
+{
+  Data* data;
+  public:
+    data_wrapper(Data* d=nullptr) : data(d) {}
+    ~data_wrapper() { delete data; }
+
+    data_wrapper(data_wrapper const&) = delete;
+    data_wrapper& operator=(data_wrapper const&) const = delete;
+    data_wrapper(data_wrapper&& other) :data(std::move(other.data))
+    {
+      other.data = nullptr;
+    }
+
+    data_wrapper& operator=(data_wrapper&& other)
+    {
+      if (this != std::addressof(other))
+      {
+        delete data;
+        data = std::move(other.data);
+        other.data = nullptr;
+      }
+      return *this;
+    };
+};
+
+template <typename T>
+void run(T val) = delete;
+void run(long val) {}
+
+struct foo
+{
+  foo() = default;
+};
+
+int main()
+{
+  
+}
