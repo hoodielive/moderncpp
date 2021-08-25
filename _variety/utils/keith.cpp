d5bc3200f11507e40468105cbdfdb07f6bbd1b39
hoodielive <hoodielive@gmail.com>
2021-08-24 22:53:50 -0400

    ...

---
+++ _variety/utils/keith.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+
+void returnKeithToConsciousness()
+{
+   std::cout << "Keith is a punk!" << std::endl; 
+}
+
+
+int main()
+{
+   returnKeithToConsciousness();
+   return 0;
+}
