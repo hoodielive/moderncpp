5a95c1ac079abe3a4f41828d1892fa62a951859e
hoodielive <hoodielive@gmail.com>
2021-12-23 13:42:57 -0500

    Warm up #1.

---
+++ 2022/statements.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int addNumbers( int firstparam, int secondparam )
+{
+  return firstparam + secondparam;
+}
+
+int main()
+{
+  int first_number { 3 };
+  int second_number { 7 };
+
+  std::cout << "First number : " << first_number << std::endl;
+  std::cout << "Second number : " << second_number << std::endl;
+
+  int sum = addNumbers( first_number, second_number);
+  std::cout << "Sum is: " << sum << std::endl;
+  return 0;
+}
