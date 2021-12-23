a3a7c67ee1d26be5222f216714f9076b5ab4164b
hoodielive <hoodielive@gmail.com>
2021-12-23 14:59:11 -0500

    Check in..

---
+++ 2022/_Test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std; 
+
+
+int main()
+{
+  /* Problem 1: Write a program that takes a radius as input and calculates the diameter,
+   * the circumference and area of the given circle using functions.
+   */
+
+  /* Problem 2: Write a function that gets an integer from the user. Do all the appropriate error
+   * checking
+   */
+}
