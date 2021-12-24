cf0b8ecd0ec6b7f3fe603c1667bae962fdf0286f
hoodielive <hoodielive@gmail.com>
2021-12-24 17:42:42 -0500

    Pointers are crazy.

--- 2022/_flirtation/_pointers.cpp
+++ 2022/_flirtation/_pointers.cpp
@@ -4,17 +4,29 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  int* p; 
+  int* p;
   int* q;
   int *a, *b, *c;
 
-  int i = 10; 
-  p = &i; // address of i assigned to p.
+  int i = 10;
 
+  // address of i assigned to p.
+  p = &i;
+  *p = 20;
+  int* p2 = p;
 
   // Deref pointers.
   cout << "Address of i: " << p << endl;
+  cout << "Address of p2 is: " << &p2 << endl;
 
+  // Sometimes its good to have a pointer that can point to a pointer.
+  // **
+
+  int** r = &p;
+
+  cout << "Address of p: " << r << endl;
+  cout << "Address of i: " << *r << endl;
+  cout << "Value of i: " << **r << endl;
 
   return 0;
 }
