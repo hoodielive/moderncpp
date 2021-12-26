095752a3909ad8a6dc5bb4bf0bea2cf58d12ddf9
hoodielive <hoodielive@gmail.com>
2021-12-26 15:53:33 -0500

    First Entry to Hangman

--- 2022/_hangman/Hangman.cpp
+++ 2022/_hangman/Hangman.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
 
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+  /*
+   * Create the program.
+   * Prompt the player to enter a phrase.
+   * The phrase will be hidden by a series of dashes.
+   * The Player can then guess letters.
+   * If the guesses are correct, all the letters which match
+   * will be revealed.
+   * If the letters or incorrect, then 1 more body part will be
+   * added to the gallows.
+   * If they lose the gallows will reveal a figure hanging.
+   * and the word should be revealed.
+   * Prompt the Player to play again.
+   */
+
+
+
+  return 0;
+}
