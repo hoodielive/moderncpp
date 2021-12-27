c9e6a11111ed283b970c17e0e9ebbe588dbef0d6
hoodielive <hoodielive@gmail.com>
2021-12-27 18:30:42 -0500

    Stack memory and thangs..

---
+++ 2022/CompSci/src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+
+  int num = 7;
+
+  // Give me the value.
+  cout << "Value: " << num << endl;
+
+  // This will return a stack memory address so:
+  // The number is going to be huge.
+
+  cout << "Address: " << &num << endl;
+
+  return 0;
+}
