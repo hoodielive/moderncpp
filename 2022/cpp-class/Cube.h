c26c319f780339de7457a9caf3294ca65b08cda5
hoodielive <hoodielive@gmail.com>
2021-12-27 14:09:43 -0500

    Cubey..

--- 2022/cpp-class/Cube.h
+++ 2022/cpp-class/Cube.h
@@ -1,11 +1,17 @@
-#pragma once
+#pragma once 
+
+// only compile once
 
 class Cube
 {
+  // API
+
   public:
     double getVolume();
     double getSurfaceArea();
     void setLength(double length);
+
+  // Only class has access to length_.
 
   private:
     double length_;
