044984987535bb26e392941efdc47cde9035f044
hoodielive <hoodielive@gmail.com>
2021-12-28 23:46:48 -0500

    Heap alloc array

---
+++ 2022/HeapPuzzle/puzzle4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+  int *x;
+  int size = 3;
+  x = new int[size];
+
+  for (int i = 0; i < size; i++)
+  {
+    x[i] = i + 3;
+
+    cout << x << endl;
+  }
+
+  delete[] x;
+
+  return 0;
+
+}
