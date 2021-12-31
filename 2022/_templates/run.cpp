ea1623b24aeab417aa0814e13e8f9efdbd35f733
hoodielive <hoodielive@gmail.com>
2021-12-31 18:19:04 -0500

    max is ambigious

--- 2022/_templates/run.cpp
+++ 2022/_templates/run.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
+using namespace std;
 
 template<typename T>
 T max(T a, T b)
 {
-  if (a < b) { return a; }
+  if (a > b) { return a; }
   return b;
 }
 
 
-int main(int argc, char *argv[])
+int main()
 {
-  max(4, 7);
+  cout << "max(3, 5): " <<  max(4, 7) << endl;
+  cout << "max('a', 'b'): " << max('a', 'b') << endl;
 
   return 0;
 }
