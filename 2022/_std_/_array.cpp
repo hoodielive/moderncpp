51fcfa036cf592fd525ebc6d3db544435f999bb7
hoodielive <hoodielive@gmail.com>
2021-12-31 22:44:33 -0500

    std::array sucks to me.

--- 2022/_std_/_array.cpp
+++ 2022/_std_/_array.cpp
@@ -5,7 +5,10 @@ using namespace std;
 
 void printMe(const std::array<int, 5> &data)
 {
-  for (int i = 0; i < data.size(););
+  for (int i = 0; i < data.size(); i++)
+  {
+    cout << data.size() << endl;
+  }
 }
 
 int main(int argc, char *argv[])
@@ -15,6 +18,7 @@ int main(int argc, char *argv[])
   data[0] = 2;
   data[4] = 4;
 
+  printMe(data);
 
   cout << "Array indices that were assigned: " << data.size() << endl;
   return 0;
