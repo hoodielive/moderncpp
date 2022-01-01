bcd83edc3d8def814ac7b509bd3f280cb61dde93
hoodielive <hoodielive@gmail.com>
2021-12-31 19:56:13 -0500

    Logica

---
+++ 2022/_LinearSequentialMemory/List.h
@@ -0,0 +1,24 @@
+#pragma once
+
+template<typename T>
+class List
+{
+  public:
+    // We are targeting this List operator [] so that later when
+    // we call 'List<int> L;' or l[0] it knows I am referring to 
+    // this function index.
+
+    const T &operator[](unsigned index);
+    void insertAtFront(const T &data);
+
+  private:
+    class ListNode
+    {
+      public:
+        const T &data;
+        ListNode *next;
+        ListNode(const T &data) : data(data), next(nullptr) {  }
+    };
+
+    ListNode *head_; /*< Head pointer for our List> */
+};
--- 2022/_LinearSequentialMemory/main.cpp
+++ 2022/_LinearSequentialMemory/main.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-
 int main()
 {
   // Create a fixed-size array of 10 primes:
