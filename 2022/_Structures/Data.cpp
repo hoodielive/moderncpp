59d7264d782bf2af0bbdba777e179499572de3c4
hoodielive <hoodielive@gmail.com>
2022-01-01 16:10:10 -0500

    User defined types

---
+++ 2022/_Structures/Data.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+struct BookType
+{
+  int bookId;
+  char title[256];
+  char author[256];
+  int year;
+  float price;
+};
+
+int main(int argc, char *argv[])
+{
+  
+  return 0;
+}
