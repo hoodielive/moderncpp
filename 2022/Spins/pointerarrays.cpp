b1382c14a6e1a1002fb822af72020bba5844359d
hoodielive <hoodielive@gmail.com>
2022-01-04 02:38:17 -0500

    Thus ends.

--- 2022/Spins/pointerarrays.cpp
+++ 2022/Spins/pointerarrays.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 
 struct College
@@ -9,6 +10,14 @@ struct College
 void print_name(College* college_ptr)
 {
   printf("%s College\n", college_ptr->name);
+}
+
+void print_names(College* colleges, size_t n_Colleges)
+{
+  for (size_t i=0; i < n_Colleges; i++)
+  {
+    printf("%s College\n", colleges[i].name);
+  }
 }
 
 int main(int argc, char *argv[])
@@ -22,5 +31,8 @@ int main(int argc, char *argv[])
 
   print_name(best_colleges);
   printf("Best colleges is: %p\n", best_colleges);
+
+  print_names(best_colleges, 4);
+
   return 0;
 }
