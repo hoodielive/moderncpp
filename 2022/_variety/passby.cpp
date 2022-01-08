d1c5bc9f8ba246b585cd23c11aaca896c5c94f70
hoodielive <hoodielive@gmail.com>
2022-01-08 16:42:43 -0500

    Pass it by const ref and reg ref.

--- 2022/_variety/passby.cpp
+++ 2022/_variety/passby.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 
 void myFunction(int& byref);
 
+void myConstFunction(const string& byconstref);
 int main(int argc, char *argv[])
 {
 
   int x = 123;
   myFunction(x);
+
+  myConstFunction("Haddy mae.");
 
   return 0;
 }
@@ -17,6 +21,11 @@ int main(int argc, char *argv[])
 void myFunction(int& byref)
 {
   byref++;
-  cout << "Argument passed by reference: " << byref;
+  cout << "Argument passed by reference: " << byref << endl;
 
+}
+
+void myConstFunction(const string& byconstref)
+{
+  cout << "Args passed by const ref: " << byconstref << '\n';
 }
