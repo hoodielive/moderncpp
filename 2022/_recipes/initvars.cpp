37b00f3167fd670208dd73742508c6304db2090e
hoodielive <hoodielive@gmail.com>
2022-01-13 17:56:11 -0500

    Text editor. check.

--- 2022/_recipes/initvars.cpp
+++ 2022/_recipes/initvars.cpp
@@ -18,24 +18,27 @@ int main()
 {
   using myVector = vector<int>;
 
-  myVector vecA( 1 );
+  myVector vecA(1);
 
   cout << vecA.size() << " " << vecA[0] << endl;
 
-  myVector vecB( 1, 10 );
+  myVector vecB(1,10);
   cout << vecB.size() << " " << endl;
+
+  myVector vecC{ 1, 10 };
+  cout << vecC.size() << " " << vecC[0] << endl;
 
   FriendlyFace objA;
   // We employ the initializer here to support uniform
   // initialization.
   FriendlyFace objB(FriendlyFace{});
 
-  int number { 0 };
+  auto number { 0 };
   // We are adding a static_cast to inform the compiler
   // of the deliberated narrowing in conversion.
-  char another { static_cast<char>(512) };
-  double bigNumber { 1.0 };
-  float littleNumber { static_cast<float>(bigNumber) };
+  auto another { static_cast<char>(512) };
+  auto bigNumber { 1.0 };
+  auto littleNumber { static_cast<float>(bigNumber) };
 
   return 0;
 }
