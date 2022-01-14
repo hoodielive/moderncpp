738ed6ae4208159e0be9010bb267de356552111f
hoodielive <hoodielive@gmail.com>
2022-01-14 17:38:36 -0500

    ...

--- 2022/_banas/_beginning/tut3.cpp
+++ 2022/_banas/_beginning/tut3.cpp
@@ -25,5 +25,20 @@ int main(int argc, char *argv[])
   printf("%d / %d = %d\n", nNum1, nNum2, (nNum1 / nNum2));
   printf("%d %% %d = %d\n", nNum1, nNum2, (nNum1 % nNum2));
 
+  /* 
+    Problem
+    Enter Miles: 5
+    5 miles equals 8.0467 kilometers.
+   */
+
+  string sMiles;
+  double dMiles, dKilometers;
+  cout << "Enter miles: ";
+  cin >> sMiles;
+
+  dMiles = stod(sMiles);
+  dKilometers = dMiles * 1.60934;
+  printf("%.1f miles equals %.4f kilometers\n", dMiles, dKilometers);
+
   return 0;
 }
