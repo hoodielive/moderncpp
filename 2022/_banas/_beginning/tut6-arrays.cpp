7e15f130c130e5ac6ea450d832a12d67293973e9
hoodielive <hoodielive@gmail.com>
2022-01-14 18:59:56 -0500

    Getting kind of hectic.

--- 2022/_banas/_beginning/tut6-arrays.cpp
+++ 2022/_banas/_beginning/tut6-arrays.cpp
@@ -27,11 +27,14 @@ int main(int argc, char *argv[])
   int num = 4;
   string isEven = (( num % 2 ) == 0) ? "Even" : "Odd";
 
-  cout << "Even or Odd : " << isEven  << endl;
+  cout << "isEven is : " << isEven  << endl;
 
   // Print out all odd numbers from 1 to 20.
 
   int selectOdds[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+  cout << "The size of selectOdds is: " << sizeof(selectOdds) << endl;
+  cout << "The size of index 0 of selectOdds is: " << sizeof(selectOdds[0]) << endl;
 
   arrSize = sizeof(selectOdds)/sizeof(selectOdds[1]);
 
@@ -44,11 +47,10 @@ int main(int argc, char *argv[])
     }
   }
 
- 
   num = 1;
   string isOdd = (( num % 1) == 1) ? "Even" : "Odd";
 
-  cout << "Even or Odd : " << isOdd << endl;
+  cout << "isOdd is Even or Odd : " << isOdd << endl;
 
   return 0;
 }
