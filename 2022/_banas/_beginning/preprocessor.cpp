77d6649e5d0b534156a21275bb8b1ee0378d0a62
hoodielive <hoodielive@gmail.com>
2022-01-14 14:05:43 -0500

    Basic types. I am so sleepy.

--- 2022/_banas/_beginning/preprocessor.cpp
+++ 2022/_banas/_beginning/preprocessor.cpp
@@ -11,6 +11,7 @@ int main(int argc, char *argv[])
     for (int i = 0; i < argc; ++i)
     {
       cout << "argv[] is an array of strings." << endl;
+      cout << argv[i] << '\n';
     }
   }
   return 0;
