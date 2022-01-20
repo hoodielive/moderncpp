05d5bb88914e89846a78620f917b7f34da95a1c5
hoodielive <hoodielive@gmail.com>
2022-01-19 22:56:52 -0500

    Static thread_local int - didn't work as expected.

---
+++ 2022/_variety/rats.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <iostream>
+
+
+
+struct RatThing
+{
+  static void powerUpRatThing(int nuclearIsotopes)
+  {
+    static thread_local int ratThingsPower;
+    ratThingsPower += nuclearIsotopes;
+    const auto wasteHeat = ratThingsPower * 20;
+
+    if (wasteHeat > 10000)
+    {
+      printf("Warning! Hot doggie!\n");
+    }
+    
+    printf("Rat-thing power: %d\n", ratThingsPower);
+  }
+};
+
+
+
+
+int main()
+{
+  RatThing::powerUpRatThing(100);
+  RatThing::powerUpRatThing(500);
+
+  return 0;
+}
