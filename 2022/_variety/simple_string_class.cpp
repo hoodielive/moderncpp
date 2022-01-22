7365107e26110dab2371e77c25a8fa6109e456a7
hoodielive <hoodielive@gmail.com>
2022-01-22 01:22:41 -0500

    Updated the String class.

--- 2022/_variety/simple_string_class.cpp
+++ 2022/_variety/simple_string_class.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 struct SimpleString
 {
-  SimpleString(size_t max_size) 
-    : max_size{ max_size }, length{} 
+  SimpleString(size_t max_size) : max_size{ max_size }, length{} 
     {
       if (max_size == 0)
       {
-        throw std::runtime_error{ "Max size must be at least 1." };
+        throw std::runtime_error{ "Max size must be at least 1 byte." };
+      }
+      else {
+        printf("Hello, thanks for playing along.");
       }
       buffer = new char[max_size];
       buffer[0] = 0;
@@ -30,5 +32,6 @@ struct SimpleString
 int main(int argc, char *argv[])
 {
   
+  SimpleString loya(3);
   return 0;
 }
