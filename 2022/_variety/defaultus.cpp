f4e1554f94c7341c24214c34f3312e8e120b499e
hoodielive <hoodielive@gmail.com>
2022-01-27 23:49:28 -0500

    Well

--- 2022/_variety/defaultus.cpp
+++ 2022/_variety/defaultus.cpp
@@ -1,4 +1,24 @@
-import <string>;
+#include <iostream>
+#include <format>
+using namespace std;
 
+export module employee;
 
+export struct Employee
+{
+  char firstInitial;
+  char lastInitial;
+  int employeeNumber;
+  int salary;
+};
 
+int main()
+{
+  Employee anEmployee;
+  anEmployee.firstInitial = 'J';
+  anEmployee.lastInitial = 'D';
+  anEmployee.employeeNumber = 'D';
+  anEmployee.salary = 800000;
+
+  cout << format("Employee: {}{}", anEmployee,firstInitial, anEmployee.lastInitial) << endl;
+}
