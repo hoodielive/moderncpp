c8c54c4fbbef5d06abc13f56bf560d3b38a21155
hoodielive <hoodielive@gmail.com>
2022-11-24 20:03:37 -0500

    A

--- 2022/arrays/main.cpp
+++ 2022/arrays/main.cpp
@@ -4,14 +4,27 @@ using namespace std;
 
 int main(int argc, char **argv) 
 {
-  int array[5] = { 1, 2, 3, 4, 5 };
+  int array[] = { 1, 2, 3, 4, 5 };
 
-  int i = 0;
+//  int i = 0;
 
-  for (i = 0; i < sizeof(array) / sizeof(int); i++)
+  // for (i = 0; i < sizeof(array) / sizeof(int); i++)
+  // {
+  //   cout << array[i]  << endl;
+  // }
+
+  for (auto arr: array)
   {
-    cout << array[i]  << endl;
+    cout << arr << endl;
   }
 
+  int array_copy[]{};
+
+  for (int i=0; i < std::size(array); i++)
+  {
+    array_copy[i] = array[i];
+    cout << array_copy[i] << endl;
+  }
+  
   return 0;
 }
