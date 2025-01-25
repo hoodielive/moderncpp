66de27c4b0c7b84de817d5878c7c45d93e9a88b5
hoodielive <hoodielive@gmail.com>
2025-01-25 15:12:59 -0500

    Pretty cool.

--- GameDevPrep/lambda.cpp
+++ GameDevPrep/lambda.cpp
@@ -1,15 +1,32 @@
 #include <print>
+#include <vector>
+#include <algorithm>
+#include <functional>
 
 using namespace std;
 
-// [capture(parameters) -> return_type { body }]
+void process(int a, int b, std::function<int(int, int)> operation) {
+  print("Result: {}.\n", operation(a, b));
+}
 
 int main()
 {
+  // [capture(parameters) -> return_type { body }]
   auto add = [](int a, int b) { return a + b; }; 
   int result = add(5, 10);
 
   print("The sum of 5 and 10 is: {}. \n", result);
 
+  vector<int> nums = { 4, 2, 8, 6 };
+  sort(nums.begin(), nums.end(), [](int a, int b) { return a < b; });
+
+  for (int num : nums)
+  {
+    print("{}", num);
+  }
+ 
+  process(5, 3, [](int a, int b) { return a + b; });
+  process(5, 3, [](int a, int b) { return a - b; });
+   
   return 0;
 }
