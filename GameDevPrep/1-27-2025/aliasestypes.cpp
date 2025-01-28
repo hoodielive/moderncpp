8fd02f5a595ce068774444110268e9ece4837dc0
hoodielive <hoodielive@gmail.com>
2025-01-27 23:16:21 -0500

    Done.

--- GameDevPrep/1-27-2025/aliasestypes.cpp
+++ GameDevPrep/1-27-2025/aliasestypes.cpp
@@ -6,11 +6,30 @@ using namespace std;
 using byte        = unsigned char;
 using byte_ptr    = unsigned char *;
 using array_t     = int[10];
-using fn          = void(byte, double);
+using fn_ptr      = void(*)(byte, double);
+
+template<typename T>
 using vec_t       = std::vector<T, custom_allocator<T>>;
 
 template <class T>
-class custom_allocator { /*...*/ };
+class custom_allocator 
+{
+  public:
+    custom_allocator() noexcept {}
+    
+    template<class U>
+    custom_allocator(const custom_allocator<U>&) noexcept {}
+
+    T* allocate(size_t n)
+    {
+      return static_cast<T*>(::operator new(n* sizeof(T)));
+    }
+
+    void deallocate(T* p, size_t) noexcept
+    {
+      ::operator delete(p);
+    }
+};
 
 template <typename T>
 
@@ -25,6 +44,6 @@ int main()
   vec_t<int> vi;
   vec_t<std::string> vs;
   
-  fn* f = func;
-  
+  fn_ptr = func;
+  delete[] pb;
 }
